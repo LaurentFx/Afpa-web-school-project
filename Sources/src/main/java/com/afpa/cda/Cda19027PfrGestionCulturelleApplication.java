dd9f8410d7d5b6e701ad4239966e9111aaa68b59
lfarineaux <lfarineaux@free.fr>
2019-12-13 15:07:33 +0100

    architecture

---
+++ Sources/src/main/java/com/afpa/cda/Cda19027PfrGestionCulturelleApplication.java
@@ -0,0 +1,13 @@
+package com.afpa.cda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cda19027PfrGestionCulturelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cda19027PfrGestionCulturelleApplication.class, args);
+	}
+
+}
