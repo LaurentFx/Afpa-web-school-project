3ce6acadbe627a6eb87d12e1d8e68bfae9e74dad
lfarineaux <lfarineaux@free.fr>
2020-01-06 18:59:46 +0100

    modif conf

---
+++ Sources/gestion-culturelle-back/src/main/java/com/afpa/cda/GestionCulturelleBackApplication.java
@@ -0,0 +1,27 @@
+package com.afpa.cda;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class GestionCulturelleBackApplication  implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCulturelleBackApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper createModelMapper() {
+		return new ModelMapper();
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH");
+	}
+	
+}
