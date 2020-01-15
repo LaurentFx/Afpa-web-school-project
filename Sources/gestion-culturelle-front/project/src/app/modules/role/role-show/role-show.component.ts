1434de9515baaf31a6687f9f3d9d61d6f1aa8afa
Mohamed Akharroub <m.akharroub@gmail.com>
2020-01-15 11:53:47 +0100

    base de donne postgres conf & front list des roles

--- Sources/gestion-culturelle-front/project/src/app/modules/role/role-show/role-show.component.ts
+++ Sources/gestion-culturelle-front/project/src/app/modules/role/role-show/role-show.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RoleDto } from 'src/app/model/roleDto';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoleService } from 'src/app/service/role.service';
 @Component({
   selector: 'app-role-show',
   templateUrl: './role-show.component.html',
   styleUrls: ['./role-show.component.css']
 })
 export class RoleShowComponent implements OnInit {
-
-  constructor() { }
-
+  role: RoleDto;
+  constructor(private route: ActivatedRoute, private roleService:RoleService, private router: Router) { }
   ngOnInit() {
+    this.role = new RoleDto();
+    let id = this.route.snapshot.params['id'];
+    this.roleService.getOne(id).subscribe(
+      res => {
+        this.role = res;
+        console.log(res);
+      }
+    );
   }
-
 }
