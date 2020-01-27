000b3bb735c4bd1023037fa907745b1ff88b32d7
lfarineaux <lfarineaux@free.fr>
2020-01-27 12:51:03 +0100

    [Front] html

--- Sources/gestion-culturelle-front/project/src/app/modules/panier/panier-show-component/panier-show.component.ts
+++ Sources/gestion-culturelle-front/project/src/app/modules/panier/panier-show-component/panier-show.component.ts
@@ -5,9 +5,10 @@ import { PanierDto } from '../../../model/panierDto';
 import { User } from '../../../model/user';
 import { CommandeService } from '../../../service/commande.service';
 import { PanierService } from '../../../service/panier.service';
-import { ManifestationService } from '../../../service/manifestation.service';
 import { AuthService } from '../../../service/auth.service';
 import { UserService } from '../../../service/user.service';
+import { RoleDto } from '../../../model/roleDto';
+
 
 @Component({
   selector: 'app-panier-show',
@@ -20,17 +21,25 @@ export class PanierShowComponent implements OnInit {
   panierDto: PanierDto;
   userDto: User;
   id: number;
-  id2: number;
   idPanier: number;
+  user: String;
+  role: RoleDto;
+  isClient: boolean;
+  isConnected: boolean;
 
-
-  constructor(private manifestationService: ManifestationService, private commandeService: CommandeService,
+  constructor(private commandeService: CommandeService,
     private panierService: PanierService, private route: ActivatedRoute,
     private router: Router, private authService: AuthService, private userService: UserService,
   ) { this.userDto = new User(); this.panierDto = new PanierDto(); }
 
 
   ngOnInit() {
+    this.isConnected = this.authService.isConnected();
+    if (this.authService.getCurrentUser()) {
+      this.isClient = this.authService.getCurrentUser().role.label === 'CLIENT';
+      this.user = this.authService.getCurrentUser().nom;
+      this.role = this.authService.getCurrentUser().role;
+    }
     this.reload();
   }
 
@@ -67,12 +76,12 @@ export class PanierShowComponent implements OnInit {
 
   }
 
-cancel(id: number) {
-  this.goHome()
-}
+  cancel(id: number) {
+    this.goHome()
+  }
 
-delete (id: number) {
-  this.id = this.route.snapshot.params['id'];
+  delete(id: number) {
+    this.id = this.route.snapshot.params['id'];
     console.log('id ' + id)
     this.commandeService.delete(id).subscribe(
       res => {
@@ -80,7 +89,7 @@ delete (id: number) {
         this.goHome()
       }
     )
-}
+  }
 
   goHome() {
 
